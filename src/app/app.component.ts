0d6b9cc5af39128e8c1ef726b9e2a922503b63f2
Yann-Thomas Le Moigne <yann.tho.le.moigne@gmail.com>
2024-10-16 14:12:04 +0200

    Dummy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'crm-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angularCRM-19';
+
+  clicked(label: string) {
+    console.log('The button was clicked', label);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppMaterialModule } from './app-material.module';
+import { DummyComponent } from './component/dummy/dummy.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, DummyComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, AppMaterialModule],
   providers: [
     provideAnimationsAsync()
